2c41d3cc211c2ec106177db371880ba6eca73a4d
Siva Sankar <attilisiva459@gmail.com>
2026-04-04 19:17:31 +0530

    updated the security config

--- src/main/java/com/sivacodes/interview_analyzer/config/SecurityConfig.java
+++ src/main/java/com/sivacodes/interview_analyzer/config/SecurityConfig.java
@@ -6,32 +6,37 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.*;
 
+import java.util.List;
+
 @Configuration
 public class SecurityConfig {
 
     @Bean
-
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http
-        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-        .csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(auth -> auth
-            .anyRequest().permitAll()
-        );
 
-    return http.build();
-}
-   
+        http
+            .csrf(csrf -> csrf.disable())
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            .authorizeHttpRequests(auth -> auth
+                .anyRequest().permitAll()   
+            );
+
+        return http.build();
+    }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("https://interview-analyzer-two.vercel.app");
-        config.addAllowedMethod("*");
-        config.addAllowedHeader("*");
+
+        config.setAllowedOriginPatterns(List.of("*")); 
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+
+        config.setAllowCredentials(false); 
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
+
         return source;
     }
 }
